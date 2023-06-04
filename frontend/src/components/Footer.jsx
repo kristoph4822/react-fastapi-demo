2e3b63cb41069cedebeb81faad524bcce17d3bdb
Krzysztof Maciejewski <krzysztof.maciejewski@dentsu.com>
2023-06-05 00:47:53 +0200

    initial commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+
+  return (
+      <footer className={"text-center p-4 bg-black mt-auto text-white"}>
+          <div >
+            WIELKA STOPA
+          </div>
+      </footer>
+  );
+}
+
+export default Footer;
